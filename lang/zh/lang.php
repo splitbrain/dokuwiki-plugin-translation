36988bc2e94f41069bbda7bb8e9b9f15b6c1542a
ZDYX <zhangduyixiong@gmail.com>
2007-10-17 21:37:31 +0200

    chinese translation
    darcs-hash:20071017193731-5aa3a-ff297cbe9e62057efd3cda9cd9919ad6343e59d2.gz

---
+++ lang/zh/lang.php
@@ -0,0 +1,3 @@
+<?php
+
+$lang['translations'] = '本页面的其他翻译';
---
+++ lang/zh/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Chinese(Simplified) language file
+ *
+ * @author ZDYX <zhangduyixiong@gmail.com>
+ */
+
+$lang['translations']  = "使用空格分隔的翻译语言列表（ISO 码）。请勿填入默认语言。";
+$lang['translationns'] = "如果您只希望本插件作用于某个特定的名称空间，请在这里写上其名称。";
+$lang['translateui']   = "整个用户界面也跟随某个页面的翻译语言而改变吗？";
+$lang['about']         = "请在此输入向用户解释翻译功能的页面的名称空间。它的链接将出现在语言选择器上。";
+
