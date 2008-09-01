7ffdeb82a8943603ef994f16005552d43b9a8188
Markus Petersen <markus@mdev.dk>
2008-09-01 20:17:26 +0200

    Danish translation added
    darcs-hash:20080901181726-e56a7-adc053f165b2aee3891917f8afe6ffe9b92664d9.gz

---
+++ lang/da/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Danish language file
+ *
+ * @author Markus Petersen <markus@mdev.dk>
+ */
+
+$lang['translations'] = 'Oversættelser af denne side';
---
+++ lang/da/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Danish language file
+ *
+ * @author Markus Petersen <markus@mdev.dk>
+ */
+
+$lang['translations']  = "Mellemrums-separeret liste a oversættelsessprog (ISO koder). Lad være med at inkludere standardsproget.";
+$lang['translationns'] = "Hvis du kun vil have oversættelser under et bestemt navnerum, indsæt det her.";
+$lang['skiptrans']     = "Hvis navnet på siden matcher dette regulære udtryk, så lad være med at vise oversættelsesmenuen.";
+$lang['translateui']   = "Skal brugerfladens sprog også skiftes i fremmedsprogets navnerum?";
+$lang['about']         = "Skriv et sidenavn her hvor oversættelsesfunktionen er forklaret for dine brugere. Siden vil blive linket til fra sprogvælgeren.";
+
