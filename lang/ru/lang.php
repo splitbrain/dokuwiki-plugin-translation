b9d024b8be8b3b579e8dff509346e556f251a2b1
Oleksiy Zagorskyi <asdmin@.quality.nauu.kiev.ua>
2010-07-18 22:35:37 +0800

    added Russian, Ukrainian translation

---
+++ lang/ru/lang.php
@@ -0,0 +1,5 @@
+﻿<?php
+
+$lang['translations'] = 'Перевод этой страницы';
+$lang['outdated']     = 'Этот перевод старее чем <a href="%s" class="wikilink1">оригинальная страница</a> и может быть не актуальным.';
+$lang['diff']         = 'Смотрите что <a href="%s" class="wikilink1">было изменено</a>.';
