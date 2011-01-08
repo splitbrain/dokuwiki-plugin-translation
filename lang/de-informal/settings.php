935dd3d8f05fb926167290d2706ddd64d981ef3e
Matthias Schulte <post@lupo49.de>
2011-01-08 12:30:17 +0100

    added de-informal and updates for de

--- lang/de-informal/settings.php
+++ lang/de-informal/settings.php
@@ -6,8 +6,8 @@ $lang['skiptrans']     = 'Wenn der Seitennamen dem regulären Ausdruck entsprich
 $lang['dropdown']      = 'Eine Auswahlliste benutzen, um die Übersetzungen anzuzeigen (zu bevorzugen bei mehr als fünf Sprachen).';
 $lang['translateui']   = 'Soll die Sprache der Benutzerschnittstelle auch in die jeweilige Fremdspache umgeschaltet werden?';
 $lang['redirectstart'] = 'Anhand des Browsers des Benutzers erkennen, welche Sprache angezeigt werden soll. (Startseite leitet in den passenden Namensraum um).';
-$lang['about']         = 'Geben Sie hier eine Seite an, welche den Mechanismus der Übersetzung erklärt. Sie wird vom Sprachumschalter verlinkt.';
+$lang['about']         = 'Gebe hier eine Seite an, welche den Mechanismus der Übersetzung erklärt. Sie wird vom Sprachumschalter verlinkt.';
 $lang['localabout']    = 'Sprachspezifische Versionen der oben angegebenen Seite (anstelle einer globalen) nutzen.';
 $lang['checkage']      = 'Warnungen von möglicherweise veralteten Übersetzungen anzeigen.';
-$lang['display']       = 'Geben Sie an welches/r Symbol/Text im Sprachumschalter angezeigt werden soll. (Die Nutzung von länderspezifischen Flaggen wird aus Gründen der Benutzbarkeit nicht empfohlen.)';
+$lang['display']       = 'Gebe hier an welches/r Symbol/Text im Sprachumschalter angezeigt werden soll. (Die Nutzung von länderspezifischen Flaggen wird aus Gründen der Benutzbarkeit nicht empfohlen.)';
 
