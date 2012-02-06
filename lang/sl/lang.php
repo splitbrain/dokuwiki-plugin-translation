5fa3b4dc568f4f23b73c14bbba0c39df22f9f98a
Andreas Gohr <andi@splitbrain.org>
2012-02-06 22:57:08 +0100

    added Slovenian translation

---
+++ lang/sl/lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['translations'] = 'Prevod trenutne strani';
+$lang['outdated']     = 'Prevod je starejši od <a href="%s" class="wikilink1">izvorne strani</a> in je zato lahko zastarel.';
+$lang['diff']         = 'Oglejte si <a href="%s" class="wikilink1">spremembe</a>.';
+?>
---
+++ lang/sl/settings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * English language file
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+$lang['translations']  = 'Space separated list of translation languages (ISO codes).';
+$lang['translationns'] = 'If you only want translations below a certain namespace, put it here.';
+$lang['skiptrans']     = 'When the pagename matches this regular expression, don\'t show the translation menu.';
+$lang['dropdown']      = 'Use a dropdown list to display the translations (recommended for more than 5 languages).';
+$lang['dropdown2']     = 'When use dropdown list display country code or flag near combobox.';
+$lang['flags']         = 'Use flags on screen instead of ISO language two letter code? If dropdown list is on show only current language flag.';
+$lang['description']   = 'Display text like "Translations of this page" in current language.';
+$lang['blankflag']     = 'Display blank image if flag not exist (instead of text).';
+$lang['translateui']   = 'Should the language of the user interface be switched in foreign language namespaces, too?';
+$lang['redirectstart'] = 'Should the start page automatically redirect into a language namespace using browser language detection?';
+$lang['about']         = 'Enter a pagename here where the translation feature is explained for your users. It will be linked from the language selector.';
+$lang['showabout']     = 'Display link to about page.';
+$lang['localabout']    = 'Uporabi prevedeno različico strani o vstavku (namesto splošne strani).';
+$lang['checkage']      = 'Opozori o zastarelem prevodu.';
+?>
