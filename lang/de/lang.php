7aace8cde2e0db50d804048d44afd37c7b3b7d9f
Andreas Gohr <andi@splitbrain.org>
2013-08-03 00:05:06 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,3 +1,10 @@
 <?php
 
-$lang['translations'] = 'Übersetzungen dieser Seite';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+$lang['translations']          = 'Übersetzungen dieser Seite';
+$lang['outdated']              = 'Diese Übersetzung ist älter als das <a href="%s" class="wikilink1">Original</a> und ist eventuell veraltet.';
+$lang['diff']                  = '<a href="%s" class="wikilink1">Änderungen</a> zeigen.';
