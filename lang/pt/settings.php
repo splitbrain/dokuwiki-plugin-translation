8e6e86c499053d9b0c2323fab3f29e667c792743
André Neves <drakferion@gmail.com>
2013-08-16 06:20:05 +0200

    translation update

---
+++ lang/pt/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author André Neves <drakferion@gmail.com>
+ */
+$lang['translations']          = 'Lista de línguas de tradução (códigos ISO) separada por espaços.';
+$lang['skiptrans']             = 'Quando o nome da página condizer com esta expressão regular, não mostrar o menu de tradução.';
+$lang['dropdown']              = 'Usar uma lista de dropdown para mostrar as traduções (recomendado para mais de 5 línguas).';
+$lang['translateui']           = 'A linguagem do interface de utilizador deve ser também alterada em namespaces de língua estrangeira?';
+$lang['redirectstart']         = 'A página inicial deve redirecionar automaticamente para um namespace de língua, usando deteção de língua do navegador web?';
+$lang['about']                 = 'Insira aqui um nome de página onde a funcionalidade de tradução é explicada aos seus utilizadores. O seletor de língua terá uma ligação para lá.';
+$lang['localabout']            = 'Usar versões localizadas da página acerca (em vez de uma página acerca global).';
+$lang['checkage']              = 'Avisar acerca de possíveis traduções desatualizadas.';
+$lang['display']               = 'Selecione o que gostaria de ver mostrado no seletor de linguagem. Note que usar bandeiras de países para seleção de linguagem não é recomendado por peritos de usabilidade.';
