bcc01f1f595bdf554b8fd711d9d1658ce0befee9
Vincent Feltz <psycho@feltzv.fr>
2013-09-05 10:40:33 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,3 +1,10 @@
 <?php
 
-$lang['translations'] = 'Traductions de cette page';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Vincent Feltz <psycho@feltzv.fr>
+ */
+$lang['translations']          = 'Traductions de cette page';
+$lang['outdated']              = 'Cette traduction est plus ancienne que <a href="%s" class="wikilink1">la page originale</a> et est peut-être dépassée.';
+$lang['diff']                  = 'Voir ce qui a <a href="%s" class="wikilink1">changé</a>.';
