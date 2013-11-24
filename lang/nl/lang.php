d02356544e92861d2fb06a993f15f9bce3efbbf7
Mark C. Prins <mprins@users.sf.net>
2013-11-24 18:02:49 +0100

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -1,6 +1,11 @@
 <?php
 
-$lang['translations'] = 'Paginavertalingen';
-$lang['outdated']     = 'Deze vertaling is ouder dan de <a href="%s" class="wikilink1">originele pagina</a> en kan verouderd zijn.';
-$lang['diff']         = 'Kijk wat er is <a href="%s" class="wikilink1">veranderd</a>.';
-?>
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Mark C. Prins <mprins@users.sf.net>
+ */
+$lang['translations']          = 'Paginavertalingen';
+$lang['outdated']              = 'Deze vertaling is ouder dan de <a href="%s" class="wikilink1">originele pagina</a> en kan verouderd zijn.';
+$lang['diff']                  = 'Kijk wat er is <a href="%s" class="wikilink1">veranderd</a>.';
+$lang['transloaded']           = 'De inhoud van deze pagina in %s is voor-geladen om de vertaling te vergemakkelijken.<br />Het is mogelijk om jouw vertaling te baseren op de volgende bestaande vertalingen: %s.';
