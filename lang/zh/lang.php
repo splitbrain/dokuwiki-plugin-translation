b4c46302cd0e77616b4883d36bf11cf277a504a5
hfl <huangfeilong@gmail.com>
2014-01-06 01:20:14 +0100

    translation update

--- lang/zh/lang.php
+++ lang/zh/lang.php
@@ -1,3 +1,10 @@
 <?php
 
-$lang['translations'] = '本页面的其他翻译';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author hfl <huangfeilong@gmail.com>
+ */
+$lang['translations']          = '本页面的其他翻译';
+$lang['outdated']              = '翻译跟<a href="%s" class="wikilink1">原始页面</a>比较起来显得有些陈旧，所以可能失效。';
+$lang['diff']                  = '查看<a href="%s" class="wikilink1">更新</a>';
