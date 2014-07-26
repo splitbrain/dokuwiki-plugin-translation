caf6e9405bf9192d01456b621fc77b12e1e5540d
Camilo Sampedro <sampedro1903@gmail.com>
2014-07-27 01:40:21 +0200

    translation update

--- lang/es/lang.php
+++ lang/es/lang.php
@@ -1,3 +1,9 @@
 <?php
 
-$lang['translations'] = 'Traducciones de esta página';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Camilo Sampedro <sampedro1903@gmail.com>
+ */
+$lang['translations']          = 'Traducciones de esta página';
+$lang['outdated']              = 'Esta traducción es más antigua que la <a href="%s" class="wikilink1">página original</a> y podría estar obsoleta.';
