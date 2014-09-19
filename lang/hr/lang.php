742ca80360ef017239b342900afedca7888b825d
Davor Turkalj <turki.bsc@gmail.com>
2014-09-19 13:05:07 +0200

    translation update

---
+++ lang/hr/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Davor Turkalj <turki.bsc@gmail.com>
+ */
+$lang['translations']          = 'Prijevodi ove stranice';
+$lang['outdated']              = 'Prijevod ove stranice je stariji od <a href="%s" class="wikilink1">originalne stranice</a> i može biti zastario.';
+$lang['diff']                  = 'Pogledajte što je <a href="%s" class="wikilink1">izmijenjeno</a>.';
+$lang['transloaded']           = 'Sadržaj ove stranice u jeziku %s je napunjeno radi lakšeg prevođenja.<br />Ali možete bazirati Vaš prijevod i prema slijedećim raspoloživim prijevodima: %s.';
