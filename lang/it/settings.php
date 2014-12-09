78005b30231c1809da3b9854759989f5c48b15e7
Torpedo <dgtorpedo@gmail.com>
2014-12-09 22:40:05 +0100

    translation update

--- lang/it/settings.php
+++ lang/it/settings.php
@@ -1,12 +1,12 @@
 <?php
+
 /**
- * Italian language file
- *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author Andreas Gohr <andi@splitbrain.org>
  * @author Diego Pierotto <ita.translations@tiscali.it>
  */
-
-$lang['translations']  = "Elenco delle lingue di traduzione separati da spazi (codici ISO). Non includere la lingua predefinita";
-$lang['translationns'] = "Scrivi qui solo se vuoi le traduzioni all'interno di una certa categoria.";
-$lang['translateui']   = "Vuoi che anche la lingua dell'interfaccia utente sia modificata in categorie della stessa lingua?";
-$lang['about']         = "Inserisci qui una pagina dove la funzione di traduzione viene spiegata agli utenti. Sarà collegata al selettore lingua.";
+$lang['translations']          = 'Elenco delle lingue di traduzione separati da spazi (codici ISO). Non includere la lingua predefinita';
+$lang['translationns']         = 'Scrivi qui solo se vuoi le traduzioni all\'interno di una certa categoria.';
+$lang['translateui']           = 'Vuoi che anche la lingua dell\'interfaccia utente sia modificata in categorie della stessa lingua?';
+$lang['about']                 = 'Inserisci qui una pagina dove la funzione di traduzione viene spiegata agli utenti. Sarà collegata al selettore lingua.';
