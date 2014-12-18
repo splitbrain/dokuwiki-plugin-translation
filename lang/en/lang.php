1d666e1cce728b4457daf4a938936b22ba98090e
Mohamd <mali4256@yahoo.co.uk>
2014-12-18 12:40:06 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,7 +1,10 @@
 <?php
 
-$lang['translations'] = 'Translations of this page';
-$lang['outdated']     = 'This translation is older than the <a href="%s" class="wikilink1">original page</a> and might be outdated.';
-$lang['diff']         = 'See what has <a href="%s" class="wikilink1">changed</a>.';
-$lang['transloaded']  = 'The contents of this page\'s translation in %s have been pre-loaded for easy translation.<br />But you can base your translation on the following existing translations: %s.';
-
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['translations']          = 'Translations of this page';
+$lang['outdated']              = 'This translation is older than the <a href="%s" class="wikilink1">original page</a> and might be outdated.';
+$lang['diff']                  = 'See what has <a href="%s" class="wikilink1">changed</a>.';
+$lang['transloaded']           = 'The contents of this page\'s translation in %s have been pre-loaded for easy translation.<br />But you can base your translation on the following existing translations: %s.';
