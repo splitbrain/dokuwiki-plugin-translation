097e85c4278c7c89483cb9d4c69acfee4f5e24b4
Jaroslav Lichtblau <jlichtblau@seznam.cz>
2015-03-28 07:45:29 +0100

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -12,6 +12,7 @@ $lang['dropdown']              = 'Použít rozbalovací seznam dostupných přek
 $lang['translateui']           = 'Mělo by se přeložit i uživatelské rozhraní při změně překladu stránky?';
 $lang['redirectstart']         = 'Má hlavní stránka automaticky přesměrovávat na dostupnou jazykovou verzi jmenného prostoru dle nastavení jazyka prohlížeče?';
 $lang['about']                 = 'Vložte jméno stránky s nápovědou ohledně možnosti překládat stránky na DokuWiki s pomoci Translation pluginu. Tento odkaz bude k dispozici z výběru přeložených jazyků.';
+$lang['localabout']            = 'Použít přeložené verze stran o aplikaci (namísto té globální).';
 $lang['checkage']              = 'Upozorňovat na možné zastaralé překlady.';
 $lang['display']               = 'Vybrat co se má zobrazovat v menu pro výběr jazyka. Experti na použitelnost webu nedoporučují zobrazování obrázků vlajek zemí pro výběr jazyka.';
 $lang['copytrans']             = 'Kopírovat výchozí jazykovou verzi do editoru pro nový překlad?';
