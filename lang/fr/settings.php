4cb0f674082c85e04937b8ac54aa10c8e526c221
Schplurtz le Déboulonné <schplurtz@laposte.net>
2015-04-07 21:45:41 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -6,15 +6,16 @@
  * @author Guy Brand <gb@isis.u-strasbg.fr>
  * @author Vincent Feltz <psycho@feltzv.fr>
  * @author NicolasFriedli <nicolas@theologique.ch>
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
 $lang['translations']          = 'Liste des langues disponibles séparées par des espaces (codes ISO).';
-$lang['translationns']         = 'Si vous souhaitez ne traduire qu\'un certain namespace, indiquez-le ici.';
+$lang['translationns']         = 'Si vous souhaitez ne traduire qu\'une certaine catégorie, indiquez-la ici.';
 $lang['skiptrans']             = 'Quand le nom de la page correspond à cette expression régulière, ne pas montrer le menu de traduction.';
 $lang['dropdown']              = 'Utiliser un menu déroulant pour afficher les traductions (recommandé pour plus de 5 langues).';
-$lang['translateui']           = 'Dans les namespace traduits, la langue de l\'interface utilisateur doit-elle aussi être changée ?';
-$lang['redirectstart']         = 'La page de départ devrait-elle rediriger vers un namespace traduit en utilisant la détection de langue du navigateur ?';
+$lang['translateui']           = 'Faut-il changer la langue de l\'interface utilisateur dans les catégories traduites ?';
+$lang['redirectstart']         = 'La page de départ devrait-elle rediriger vers une catégorie traduite en utilisant la détection de langue du navigateur ?';
 $lang['about']                 = 'Entrez ici un nom de page où la fonctionnalité de traduction est expliquée aux utilisateurs. Elle sera accessible depuis le sélecteur de langue.';
 $lang['localabout']            = 'Utiliser des versions traduites de la page à propos (au lieu d\'une page à propos globale).';
 $lang['checkage']              = 'Avertir de la possibilité de traductions dépassées.';
-$lang['display']               = 'Sélectionnez ce que vous voudriez afficher dans le sélecteur de langue. Notez qu\'utiliser les drapeaux de pays pour la sélection de langue n\'est pas recommandée par les experts en ergonomie.';
-$lang['copytrans']             = 'Copier le texte en langue souce dans l\'éditeur quand une nouvelle traduction est lancée?';
+$lang['display']               = 'Sélectionnez ce que vous voudriez afficher dans le sélecteur de langue. Notez qu\'utiliser les drapeaux de pays pour la sélection de langue n\'est pas recommandé par les experts en ergonomie.';
+$lang['copytrans']             = 'Copier le texte en langue source dans l\'éditeur quand une nouvelle traduction est lancée ?';
