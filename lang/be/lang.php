a55638ce130b5011c08f03beb8b4b523d93f3974
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-07-20 13:50:48 +1000

    Create lang.php

---
+++ lang/be/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * Belarusian language file
+ *
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['translations'] = 'Пераклад гэтай старонкі';
+$lang['outdated']     = 'Гэты пераклад старэй, чым <a href="%s" class="wikilink1">арыгінальная старонка</a> і можа быць не актуальным.';
+$lang['diff']         = 'Глядзіце <a href="%s" class="wikilink1">было зменена</a>.';
+$lang['transloaded']  = 'Сэнс перакладу гэтай старонкі %s быў папярэдне загружаны для зручнасці перакладу. <br /> Таксама можаце выкарыстоўваць наступныя пераклады: %s.';
+$lang['menu']         = 'састарэлыя і адсутныя пераклады';
+$lang['missing']      = 'Адсутнічае!';
+$lang['old']          = 'застарэлы';
+$lang['current']      = 'бягучы';
+$lang['path']         = 'Шлях';
