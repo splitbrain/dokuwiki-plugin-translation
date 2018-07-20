20520847e685625cc427139ae89ab7cc9bcbd853
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-07-20 13:24:28 +1000

    Update lang.php

--- lang/es/lang.php
+++ lang/es/lang.php
@@ -1,18 +1,22 @@
 <?php
 
 /**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * Spanish language file
  *
- * @author Domingo Redal <docxml@gmail.com>
  * @author Camilo Sampedro <sampedro1903@gmail.com>
  * @author carlos <carloscyb@gmail.com>
+ * @author Domingo Redal <docxml@gmail.com>
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
-$lang['translations']          = 'Traducciones de esta página';
-$lang['outdated']              = 'Esta traducción es más antigua que la <a href="%s" class="wikilink1">página original</a> y podría estar obsoleta.';
-$lang['diff']                  = 'Ver lo que <a href="%s" class="wikilink1">ha cambiado</a>.';
-$lang['transloaded']           = 'Los contenidos de la traducción de esta página en %s han sido precargados para facilitar la traducción.<br />Pero puedes basar tu traducción en las siguientes traducciones existentes: %s.';
-$lang['menu']                  = 'traducciones obsoletas y ausentes';
-$lang['missing']               = '¡Ausente!';
-$lang['old']                   = 'obsoleta';
-$lang['current']               = 'actualizado';
-$lang['path']                  = 'ruta';
+
+$lang['translations'] = 'Traducciones de esta página';
+$lang['outdated']     = 'Esta traducción es más antigua que la <a href="%s" class="wikilink1">página original</a> y podría estar obsoleta.';
+$lang['diff']         = 'Ver lo que <a href="%s" class="wikilink1">ha cambiado</a>.';
+$lang['transloaded']  = 'Los contenidos de la traducción de esta página en %s han sido precargados para facilitar la traducción.<br />Pero puedes basar tu traducción en las siguientes traducciones existentes: %s.';
+$lang['menu']         = 'traducciones obsoletas y ausentes';
+$lang['missing']      = '¡Ausente!';
+$lang['old']          = 'obsoleta';
+$lang['current']      = 'actualizado';
+$lang['path']         = 'Ruta';
