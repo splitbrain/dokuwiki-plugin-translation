133b63528abedd03910c55558e82bb0eccf6d08d
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-07-20 14:00:11 +1000

    Create lang.php

---
+++ lang/id/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * Indonesian language file
+ *
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['translations'] = 'Terjemahan dari halaman ini';
+$lang['outdated']     = 'Ini adalah terjemahan yang lebih tua dari <a href="%s" class="wikilink1">halaman asli</a> dan mungkin menjadi usang.';
+$lang['diff']         = 'Melihat apa yang telah <a href="%s" class="wikilink1">berubah</a>.';
+$lang['transloaded']  = 'Isi dari halaman ini adalah terjemahan dalam %s telah pre-loaded untuk terjemahan mudah.<br />Tetapi anda dapat menjadi dasar dalam terjemahan berikut ini ada terjemahan: %s.';
+$lang['menu']         = 'usang dan terjemahan hilang';
+$lang['missing']      = 'Hilang!';
+$lang['old']          = 'usang';
+$lang['current']      = 'up-to-date';
+$lang['path']         = 'Path';
