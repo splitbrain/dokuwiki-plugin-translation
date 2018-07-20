8ac72e977b75d99e2199ee9b531b0a080155b8ef
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-07-20 13:46:48 +1000

    Update settings.php

--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -1,15 +1,24 @@
 <?php
 
 /**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * Turkish language file
  *
  * @author İlker R. Kapaç <irifat@gmail.com>
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
-$lang['translations']          = 'Tercüme dillerinin listesi. (boşluk ile ayrılmış, ISO kodları)';
-$lang['translationns']         = 'Eğer tercümelerin bir isim alanın (namespace) altında olmasını istiyorsanız, buraya yazın.';
-$lang['skiptrans']             = 'İsim alanı (Namespace) buradaki tanıma uyduğunda, tercüme arayüzünü gösterme.';
-$lang['dropdown']              = 'Dilleri listelemek için açılır arayüz kullan. (5\'ten fazla dil olduğunda kullanılması önerilir)';
-$lang['localabout']            = 'Bir tane genel "hakkında" sayfası kullanmak yerine, yerelleştirilmiş "hakkında" sayfaları kullan. ';
-$lang['checkage']              = 'Eski tarihli tercümeler hakkında uyarı göster.';
-$lang['display']               = 'Dil seçiminde görünmesini istediklerinizi seçin. Lütfen unutmayın, dil seçiminde ülke bayrağı kullanmak, erişilebilirlik uzmanları tarafından tavsiye edilmez.';
-$lang['copytrans']             = 'Yeni tercümeye başlarken orjinal dildeki metin, düzenleme ekranına kopyalansın mı?';
+
+$lang['translations']  = 'Tercüme dillerinin listesi. (boşluk ile ayrılmış, ISO kodları)';
+$lang['translationns'] = 'Eğer tercümelerin bir isim alanın (namespace) altında olmasını istiyorsanız, buraya yazın.';
+$lang['skiptrans']     = 'İsim alanı (Namespace) buradaki tanıma uyduğunda, tercüme arayüzünü gösterme.';
+$lang['dropdown']      = 'Dilleri listelemek için açılır arayüz kullan. (5\'ten fazla dil olduğunda kullanılması önerilir)';
+$lang['translateui']   = 'Kullanıcı arabiriminin dili de yabancı dil ad alanlarında değiştirilmelidir mi?';
+$lang['redirectstart'] = 'Başlangıç Sayfası, tarayıcı dili algılamasını kullanarak bir dil Ad alanına otomatik olarak yeniden yönlendirmeli mi?';
+$lang['about']         = 'Kullanıcılarınız için çeviri özelliğinin açıklandığı bir pagename girin. Dil seçicisinden bağlanacaktır.';
+$lang['localabout']    = 'Bir tane genel "hakkında" sayfası kullanmak yerine, yerelleştirilmiş "hakkında" sayfaları kullan. ';
+$lang['checkage']      = 'Eski tarihli tercümeler hakkında uyarı göster.';
+$lang['display']       = 'Dil seçiminde görünmesini istediklerinizi seçin. Lütfen unutmayın, dil seçiminde ülke bayrağı kullanmak, erişilebilirlik uzmanları tarafından tavsiye edilmez.';
+
+$lang['copytrans']     = 'Yeni tercümeye başlarken orjinal dildeki metin, düzenleme ekranına kopyalansın mı?';
+$lang['show_path']     = 'Eksik çeviri sayfasında yolu göster?';
