47161baa5e4173f57a22dd6a64043987e53f8876
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-07-20 14:00:31 +1000

    Create settings.php

---
+++ lang/pl/settings.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * Polish language file
+ *
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['translations']  = 'Lista obsługiwanych języków tłumaczenia (двухсимвольные kody ISO). Podzielenie wartości spacjami.';
+$lang['translationns'] = 'Jeśli chcesz przetłumaczyć tylko pewną przestrzeń nazw, to wpisz tutaj jego nazwę.';
+$lang['skiptrans']     = 'Jeśli nazwa strony pasuje do tego wyrażenia regularnego, to nie wyświetla menu tłumaczenia.';
+$lang['dropdown']      = 'Użyj listy rozwijanej, aby wyświetlić dostępne tłumaczeń (zalecane, jeśli ponad 5 tłumaczeń)';
+$lang['translateui']   = 'czy język interfejsu użytkownika można również przełączać się według języka przestrzeni nazw?';
+$lang['redirectstart'] = 'czy strona startowa automatycznie przekierowywane na przestrzeni nazw języka, za pomocą automatyczne wykrywanie języka przeglądarki?';
+$lang['about']         = 'Wpisz tutaj nazwę strony, na której zostaną wyjaśnione funkcje tłumaczenia dla swoich użytkowników. Będzie ona związana z wyborem języka.';
+$lang['localabout']    = 'Użyj zlokalizowaną wersję strony wyjaśnień (zamiast jednej globalnej strony wyjaśnień).';
+$lang['checkage']      = 'Pokaż ostrzeżenie o możliwości неактуальности tłumaczenia?';
+$lang['display']       = 'Wybierz, co chciałbyś zobaczyć w polu wyboru języka. Należy pamiętać, że korzystanie z obrazu flagi państwowej w polu wyboru języków nie było zalecane przez ekspertów w dziedzinie konsumentów udogodnienia.';
+
+$lang['copytrans']     = 'Kopiuj tekst źródłowy w okno edycji podczas tworzenia nowego tłumaczenia?';
+$lang['show_path']     = 'Pokazuj drogę na nieprzetłumaczone stronach?';
