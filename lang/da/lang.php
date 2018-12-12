d1c823bdfbeae3494d1ffd703c42cecdfa17b56d
Jacob Palm <mail@jacobpalm.dk>
2018-12-12 20:01:20 +0100

    translation update

--- lang/da/lang.php
+++ lang/da/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Jacob Palm <mail@jacobpalm.dk>
  * @author Markus Petersen <markus@mdev.dk>
  * @author Soren Birk <soer9648@eucl.dk>
  */
@@ -10,3 +11,8 @@ $lang['translations']          = 'Oversættelser af denne side';
 $lang['outdated']              = 'Denne oversættelse er ældre end den <a href="%s" class="wikilink1">originale side</a> og er muligvis forældet.';
 $lang['diff']                  = 'Se hvad der er <a href="%s" class="wikilink1">ændret</a>.';
 $lang['transloaded']           = 'Indholdet af denne sides oversættelse i %s er blevet præ-indlæst for lettere oversættelse. <br />Du kan basere din oversættelse på følgende nuværende oversættelser: %s.';
+$lang['menu']                  = 'forældede og manglende oversættelser';
+$lang['missing']               = 'Mangler!';
+$lang['old']                   = 'forældet';
+$lang['current']               = 'opdateret';
+$lang['path']                  = 'Sti';
