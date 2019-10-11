6c5d3c4ba5f644169373d3ddc75c2723500894ba
Bartek S <sadupl@gmail.com>
2019-10-11 10:10:14 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['translations']          = 'Tłumaczenie tej strony';
+$lang['outdated']              = 'Tłumaczenie jest starsze niż <a href="%s" class="wikilink1">oryginalna strona</a> i może być przestarzałe.';
+$lang['diff']                  = 'Zobacz co się <a href="%s" class="wikilink1">zmieniło</a>.';
+$lang['menu']                  = 'przestarzałe i brakujące tłumaczenia';
+$lang['missing']               = 'Brakujące!';
+$lang['old']                   = 'przestarzałe';
+$lang['current']               = 'aktualny';
+$lang['path']                  = 'Ścieżka';
