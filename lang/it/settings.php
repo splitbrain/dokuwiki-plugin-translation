a5bb0be07bf61a28f027a0bd776038d1d139a4f0
giordano boschetti <giordanoboschetti@gmail.com>
2020-07-02 16:45:38 +0200

    translation update

--- lang/it/settings.php
+++ lang/it/settings.php
@@ -2,19 +2,21 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author giordano boschetti <giordanoboschetti@gmail.com>
  * @author Andreas Gohr <andi@splitbrain.org>
  * @author Diego Pierotto <ita.translations@tiscali.it>
  * @author Sebastiano Pistore <olatusrooc@virgilio.it>
- * @author Sebastiano Pistore <sebastiano.pistore.info@aol.com>
- * @author OlatusRooc <olatusrooc@virgilio.it>
  */
 $lang['translations']          = 'Elenco delle lingue di traduzione separati da spazi (codici ISO). Non includere la lingua predefinita';
 $lang['translationns']         = 'Scrivi qui solo se vuoi le traduzioni all\'interno di una certa categoria.';
 $lang['skiptrans']             = 'Quando i nomi delle pagine corrispondono a questa espressione regolare non mostrare il menu di traduzione.';
 $lang['dropdown']              = 'Utilizza un menu a tendina per visualizzare le traduzioni (consigliato quando si lavora con più di cinque lingue).';
 $lang['translateui']           = 'Vuoi che anche la lingua dell\'interfaccia utente sia modificata in categorie della stessa lingua?';
+$lang['redirectstart']         = 'La pagina iniziale dovrebbe reindirizzare automaticamente nel namespace della lingua utilizzando il rilevamento della lingua del browser?';
 $lang['about']                 = 'Inserisci qui una pagina dove la funzione di traduzione viene spiegata agli utenti. Sarà collegata al selettore lingua.';
 $lang['localabout']            = 'Mostra le versioni localizzate della pagina About.';
 $lang['checkage']              = 'Avvisa della possibile presenza di traduzioni obsolete.';
+$lang['display']               = 'Seleziona cosa ti piace vedere nel selettore lingua. Nota che usare il country flag per la selezione della lingua non è raccomandato dagli esperti di usabilità.';
 $lang['copytrans']             = 'Copia nell\'editor il testo in lingua originale quando viene iniziata una nuova traduzione?';
+$lang['show_path']             = 'Visualizzare il percorso nelle pagine senza traduzione?';
