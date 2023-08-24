8458193f877342e4ef7cdaa2f544b281163ab64b
PC Services <contact@pcservices72.fr>
2023-08-24 12:20:06 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -10,10 +10,10 @@
  */
 $lang['translations']          = 'Traductions de cette page';
 $lang['outdated']              = 'Cette traduction est plus ancienne que <a href="%s" class="wikilink1">la page originale</a> et est peut-être dépassée.';
-$lang['old']                  = 'dépassée';
 $lang['diff']                  = 'Voir ce qui a <a href="%s" class="wikilink1">changé</a>.';
 $lang['transloaded']           = 'Le contenu de cette page en %s a été pré-chargé pour faciliter la traduction.<br/> Mais vous pouvez baser votre traduction sur les traductions existantes: %s';
 $lang['menu']                  = 'traductions dépassées et manquantes';
 $lang['missing']               = 'Manquante!';
+$lang['old']                   = 'dépassée';
 $lang['current']               = 'à jour';
 $lang['path']                  = 'Chemin';
