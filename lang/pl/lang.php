8f13eeecd4a6621b8018c143fda3ad0a320ab60d
Remi Fiedler <migius@gmx.net>
2024-01-22 03:40:06 +0100

    translation update

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -3,11 +3,13 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Remi Fiedler <migius@gmx.net>
  * @author Bartek S <sadupl@gmail.com>
  */
 $lang['translations']          = 'Tłumaczenie tej strony';
 $lang['outdated']              = 'Tłumaczenie jest starsze niż <a href="%s" class="wikilink1">oryginalna strona</a> i może być przestarzałe.';
 $lang['diff']                  = 'Zobacz co się <a href="%s" class="wikilink1">zmieniło</a>.';
+$lang['transloaded']           = 'Zawartość tłumaczenia tej strony w %s została wstępnie załadowana, aby ułatwić tłumaczenie.<br />Możesz jednak oprzeć swoje tłumaczenie na następujących istniejących tłumaczeniach: %s.';
 $lang['menu']                  = 'przestarzałe i brakujące tłumaczenia';
 $lang['missing']               = 'Brakujące!';
 $lang['old']                   = 'przestarzałe';
--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -3,7 +3,18 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Remi Fiedler <migius@gmx.net>
  * @author Bartek S <sadupl@gmail.com>
  */
 $lang['translations']          = 'Lista języków tłumaczenia rozdzielona spacjami (kody ISO).';
 $lang['translationns']         = 'Jeśli tylko chcesz tłumaczenia poniższej przestrzeni, umieść je tutaj.';
+$lang['skiptrans']             = 'Jeśli nazwa strony pasuje do tego regular expression, nie pokazuj menu tłumaczenia.';
+$lang['dropdown']              = 'Użyj listy rozwijanej, aby wyświetlić tłumaczenia (zalecane dla więcej niż 5 języków).';
+$lang['translateui']           = 'Czy język interfejsu użytkownika powinien być przełączany także w obcojęzycznych przestrzeniach nazw?';
+$lang['redirectstart']         = 'Czy strona startowa powinna automatycznie przekierowywać do przestrzeni nazw językowych przy użyciu wykrywania języka przeglądarki?';
+$lang['about']                 = 'Wpisz tutaj nazwę strony, na której wyjaśniono użytkownikom funkcję tłumaczenia. Zostanie ona powiązana z selektorem języka.';
+$lang['localabout']            = 'Używaj zlokalizowanych wersji strony instruktażowej (zamiast jednej globalnej strony instruktażowej).';
+$lang['checkage']              = 'Ostrzegaj o ewentualnych nieaktualnych tłumaczeniach.';
+$lang['display']               = 'Wybierz, co chcesz wyświetlić w selektorze języka. Należy pamiętać, że eksperci ds. użyteczności nie zalecają używania flag krajów do wyboru języka.';
+$lang['copytrans']             = 'Wkopiuj tekst w języku oryginalnym do edytora, rozpoczynając nowe tłumaczenie.';
+$lang['show_path']             = 'Pokaż ścieżkę na brakującej stronie tłumaczenia.';
