200480b01bb596ce49ff8807a1c8f06e355266d5
diyarbakir'\''da kaliteli eskort <lucretiatrowbridge@yahoo.com>
2024-06-13 22:15:42 +0200

    translation update

--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -3,13 +3,27 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author diyarbakir'da kaliteli eskort <lucretiatrowbridge@yahoo.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['translations']          = 'Tercüme dillerinin listesi. (boşluk ile ayrılmış, ISO kodları)';
-$lang['translationns']         = 'Eğer tercümelerin bir isim alanın (namespace) altında olmasını istiyorsanız, buraya yazın.';
-$lang['skiptrans']             = 'İsim alanı (Namespace) buradaki tanıma uyduğunda, tercüme arayüzünü gösterme.';
-$lang['dropdown']              = 'Dilleri listelemek için açılır arayüz kullan. (5\'ten fazla dil olduğunda kullanılması önerilir)';
-$lang['localabout']            = 'Bir tane genel "hakkında" sayfası kullanmak yerine, yerelleştirilmiş "hakkında" sayfaları kullan. ';
-$lang['checkage']              = 'Eski tarihli tercümeler hakkında uyarı göster.';
-$lang['display']               = 'Dil seçiminde görünmesini istediklerinizi seçin. Lütfen unutmayın, dil seçiminde ülke bayrağı kullanmak, erişilebilirlik uzmanları tarafından tavsiye edilmez.';
-$lang['copytrans']             = 'Yeni tercümeye başlarken orjinal dildeki metin, düzenleme ekranına kopyalansın mı?';
+$lang['translations']          = 'Thanks  for another informative blog. The place else could I am getting that kind of information written in such a 
+perfect means? I have a undertaking tthat I\'m 
+simply noww working on, and I\'ve been at the look out for such information.';
+$lang['translationns']         = 'Lucretia';
+$lang['skiptrans']             = 'Thanks  for another informative blog. The place else could I am 
+getting that kind oof information written in such a 
+perfect means? I have a undertaking that I\'m simply now working on, and I\'ve been at the look out for such information.';
+$lang['dropdown']              = 'Thanks  for another informative blog. The plce else could I am 
+getting that kind of information written in such a perfect means?
+I have a undertaking that I\'m simply now working on, and I\'ve been at the llok out for such information.';
+$lang['translateui']           = 'Lucretia';
+$lang['redirectstart']         = 'Trowbridge';
+$lang['about']                 = 'His name is Zachary Cantara but he doesn\'t like folks use his full person\'s name. The favorite hobby for my children and me is perform rock and roll there isn\'t anything would never give upward. Years ago she gone to live in Guam. The job she\'s been occupying a long time is a credit authoriser but soon she\'ll be on her own. Check out her website here: https://Bnhlaw.Co.kr/bbs/board.php?bo_table=free&wr_id=7992';
+$lang['localabout']            = 'His name is Zachary Cantara but he doesn\'t like folks use his full person\'s name. The favorite hobby for my children and me is perform rock and roll there isn\'t anything would never give upward. Years ago she gone to live in Guam. The job she\'s been occupying a long time is a credit authoriser but soon she\'ll be on her own. Check out her website here: https://Bnhlaw.Co.kr/bbs/board.php?bo_table=free&wr_id=7992';
+$lang['checkage']              = 'His name is Zachary Cantara but he doesn\'t like folks use his full person\'s name. The favorite hobby for my children and me is perform rock and roll there isn\'t anything would never give upward. Years ago she gone to live in Guam. The job she\'s been occupying a long time is a credit authoriser but soon she\'ll be on her own. Check out her website here: https://Bnhlaw.Co.kr/bbs/board.php?bo_table=free&wr_id=7992';
+$lang['display']               = 'Australia';
+$lang['copytrans']             = 'Thanks  for another informative blog. The place else could I am ggetting 
+that kind oof information written in such a perfect means? 
+I have a undertaking that I\'m simply now working on, andd I\'ve been at the look out for such information.';
+$lang['show_path']             = 'Thanks  for another informative blog. The place else could I am getting that kind of information written in such a erfect means?
+I have a undertaking that I\'m simply now working on, and I\'ve been at the look out for such information.';
