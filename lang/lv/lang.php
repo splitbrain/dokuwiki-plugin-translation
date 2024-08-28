30a2a0a1b01810eeac3a7cf7e281fba1f6272fcd
https://www.google.me/url?q=https://globalarticlewarehouse.com <lenore.wentworth@bigpond.com>
2024-08-29 00:35:06 +0200

    translation update

--- lang/lv/lang.php
+++ lang/lv/lang.php
@@ -2,10 +2,33 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author https://www.google.me/url?q=https://globalarticlewarehouse.com <lenore.wentworth@bigpond.com>
  * @author Aivars Miška <allefm@gmail.com>
  */
-$lang['translations']          = 'Citās valodās';
-$lang['outdated']              = 'Šis tulkojums ir vecāks par <a href="%s" class="wikilink1">oriģinālo lapu</a> un varbūt ir novecojis.';
-$lang['diff']                  = 'Redzēt, ka ir <a href="%s" class="wikilink1">mainījies</a>.';
-$lang['transloaded']           = 'Vieglākai tulkošanai ir ielādēts lapas saturs  no %s .<br />Bet varat balstīties arī uz šādiem tulkojumiem: %s.';
+$lang['translations']          = 'Make use of write-up marketing strategies to get backlinks to your contents.
+In case you need to confirm what number of inbound hyperlinks you are receiving 
+then it is a good suggestion to utilize a backlink checker.';
+$lang['outdated']              = 'https://www.google.me/url?q=https://globalarticlewarehouse.com/';
+$lang['diff']                  = 'https://www.google.me/url?q=https://globalarticlewarehouse.com/';
+$lang['transloaded']           = 'Make use of write-up marketing strategies to get backlinks to your 
+contents. In case you need to confirm what number of inbound hyperlinks you are receiving 
+then it is a good suggestion to utilize a backlink 
+checker.';
+$lang['menu']                  = 'Make use of write-up marketing strategies to get backlinks to your contents.
+In case you need to confirm what number of 
+inbound hyperlinks you are receiving then it is a good suggestion to utilize 
+a backlink checker.';
+$lang['missing']               = 'Make use of write-up marketing strategies to get backlinks to your contents.
+In case you need to confirm what number of inbound hyperlinks 
+you are receiving then it is a good suggestion to utilize a backlink checker.';
+$lang['old']                   = 'Make use of write-up marketing strategies to get 
+backlinks to your contents. In case you need to confirm what number of inbound hyperlinks you are receiving then it is a good suggestion to 
+utilize a backlink checker.';
+$lang['current']               = 'Make use of write-up marketing strategies to get backlinks to 
+your contents. In case you need to confirm what number of 
+inbound hyperlinks you are receiving then it is a good suggestion to utilize a backlink 
+checker.';
+$lang['path']                  = 'Make use of write-up marketing strategies to get backlinks to your contents.
+In case you need to confirm what number of inbound hyperlinks you are receiving then it is a good suggestion to utilize a backlink 
+checker.';
--- lang/lv/settings.php
+++ lang/lv/settings.php
@@ -2,18 +2,31 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author Aivars Miška <allefm@gmail.com>
+ *
+ * @author https://www.google.me/url?q=https://globalarticlewarehouse.com <lenore.wentworth@bigpond.com>
  * @author Aivars Miška <allefm@gmail.com>
  */
-$lang['translations']          = 'Ar atstarpēm atdalīts tulkojumu valodu saraksts  (ISO kodi). Izņemot noklusēto valodu.';
-$lang['translationns']         = 'Ja tulkojumus vajag tikai noteiktā nodaļā, ieraksti to šeit.';
-$lang['skiptrans']             = 'Ja lapas nosaukums atbilst regulārajai izteiksmei, tulkošanas izvēlni nerādīt.';
-$lang['dropdown']              = 'Lietot izkrītošo izvēlni tulkojumu parādīšanai (ieteikt, ja ir vairāk par 5 valodām). ';
-$lang['translateui']           = 'Vai svešvalodu nodaļās jāpārslēdz arī lietotāja sakarnes valoda?';
-$lang['redirectstart']         = 'Vai sākuma lapai automātiski jāpārslēdzas atkarībā no pārlūkprogrammas noteiktās valodas?';
-$lang['about']                 = 'Ieraksti šeit lapu, kurā lietotājiem izskaidrotas tulkošas iespējas. Tā tiks piesaistīta valodu izvēlei.';
-$lang['localabout']            = 'Lietot "par" lapas lokalizēto versiju, nevis globālo "par" lapu.';
-$lang['checkage']              = 'Brīdināt pa varbūt novecojušiem tulkojumiem. ';
-$lang['display']               = 'Norādiet, ko lietot valodas izvēlei. Ņemiet vērā, ka valodām izmantot valstu karogus neiesaka.';
-$lang['copytrans']             = 'Sākot tulkojumu, iekopēt redaktorā oriģināltekstu?';
+$lang['translations']          = 'Make use of write-up marketing strategies to get backlinks 
+to your contents. In case you need to confirm what number of 
+inbound hyperlinks you are receiving then it is a good suggestion to 
+utilize a backlink checker.';
+$lang['translationns']         = 'Lenore';
+$lang['skiptrans']             = 'Make use of write-up marketing strategies to get backlinks to your 
+contents. In case you need to confirm what number of 
+inbound hyperlinks you are receiving then it is a good suggestion to utilize 
+a backlink checker.';
+$lang['dropdown']              = 'Make use of write-up marketing strategies to get backlinks to your contents.
+In case you need to confirm what number of inbound hyperlinks you are receiving then it is a good suggestion to utilize 
+a backlink checker.';
+$lang['translateui']           = 'Lenore';
+$lang['redirectstart']         = 'Wentworth';
+$lang['about']                 = '58 yr old Gallery or Museum Technician Murry from Mount Albert, enjoys juggling, live girls and operating on cars. During the last year has made a journey to Sceilg Mhichíl.';
+$lang['localabout']            = '58 yr old Gallery or Museum Technician Murry from Mount Albert, enjoys juggling, live girls and operating on cars. During the last year has made a journey to Sceilg Mhichíl.';
+$lang['checkage']              = '58 yr old Gallery or Museum Technician Murry from Mount Albert, enjoys juggling, live girls and operating on cars. During the last year has made a journey to Sceilg Mhichíl.';
+$lang['display']               = 'Netherlands';
+$lang['copytrans']             = 'Make use of write-up marketing strategies to get backlinks to 
+your contents. In case you need to confirm what number of inbound hyperlinks you are receiving then it is a good 
+suggestion to utilize a backlink checker.';
+$lang['show_path']             = 'Make use of write-up marketing strategies to get backlinks to your contents.
+In case you need to confirm what number of inbound hyperlinks you are receiving then it 
+is a good suggestion to utilize a backlink checker.';
