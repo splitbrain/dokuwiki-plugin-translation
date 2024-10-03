0f386a661c1791f85707fa4d106b68f1a2b7f2c9
реклама Челны вакансии <darrylrichmond@yahoo.com>
2024-10-03 09:20:18 +0200

    translation update

--- lang/sl/settings.php
+++ lang/sl/settings.php
@@ -2,17 +2,75 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author реклама Челны вакансии <darrylrichmond@yahoo.com>
  * @author Andreas Gohr <andi@splitbrain.org>
  * @author Matej Urbančič <mateju@svn.gnome.org>
  */
-$lang['translations']          = 'Space separated list of translation languages (ISO codes).';
-$lang['translationns']         = 'If you only want translations below a certain namespace, put it here.';
-$lang['skiptrans']             = 'When the pagename matches this regular expression, don\'t show the translation menu.';
-$lang['dropdown']              = 'Use a dropdown list to display the translations (recommended for more than 5 languages).';
-$lang['translateui']           = 'Should the language of the user interface be switched in foreign language namespaces, too?';
-$lang['redirectstart']         = 'Should the start page automatically redirect into a language namespace using browser language detection?';
-$lang['about']                 = 'Enter a pagename here where the translation feature is explained for your users. It will be linked from the language selector.';
-$lang['localabout']            = 'Uporabi prevedeno različico strani o vstavku (namesto splošne strani).';
-$lang['checkage']              = 'Opozori o zastarelem prevodu.';
-$lang['display']               = 'Izbor možnosti za prikaz jezika v izbirniku jezika. Izbor zastave jezika v izbiri ni priporočen.';
+$lang['translations']          = 'Группа объявления Набережные челны в telegram.
+Постинг частных объявлений бесплатно!
+
+Рекламные и коммерческие объявления, по правилам группы.
+
+#НабережныеЧелны #ОбъявленияНабережныеЧелны #Челны #БесплатныеОбъявления #объявление #доскаобъявлений #барахолка #телеграм #телеграмм #telegram
+Присоединяйся, чтобы не потерять!!!
+реклама Челны вакансии
+Группы городов России опубликованы здесь!
+
+папки в телеграмме';
+$lang['translationns']         = 'Darryl';
+$lang['skiptrans']             = 'Группа объявления Набережные челны в telegram.
+Постинг частных объявлений бесплатно!
+
+
+Рекламные и коммерческие объявления, по правилам группы.
+
+#НабережныеЧелны #ОбъявленияНабережныеЧелны #Челны #БесплатныеОбъявления #объявление #доскаобъявлений #барахолка 
+#телеграм #телеграмм #telegram
+Присоединяйся, чтобы не потерять!!!
+реклама Челны вакансии
+Группы городов России опубликованы здесь!
+
+папки в телеграмме';
+$lang['dropdown']              = 'Группа объявления Набережные челны в telegram.
+Постинг частных объявлений бесплатно!
+
+Рекламные и коммерческие объявления, по 
+правилам группы.
+#НабережныеЧелны #ОбъявленияНабережныеЧелны #Челны #БесплатныеОбъявления 
+#объявление #доскаобъявлений #барахолка #телеграм 
+#телеграмм #telegram
+Присоединяйся, чтобы не потерять!!!
+
+реклама Челны вакансии
+Группы городов России опубликованы здесь!
+папки в телеграмме';
+$lang['translateui']           = 'Darryl';
+$lang['redirectstart']         = 'Richmond';
+$lang['about']                 = 'Загляните в мою телеграмм группу; реклама Челны вакансии';
+$lang['localabout']            = 'Загляните в мою телеграмм группу; реклама Челны вакансии';
+$lang['checkage']              = 'Загляните в мою телеграмм группу; реклама Челны вакансии';
+$lang['display']               = 'Netherlands';
+$lang['copytrans']             = 'Группа объявления Набережные челны в telegram.
+Постинг частных объявлений бесплатно!
+
+Рекламные и коммерческие объявления, по правилам 
+группы.
+#НабережныеЧелны #ОбъявленияНабережныеЧелны 
+#Челны #БесплатныеОбъявления #объявление #доскаобъявлений 
+#барахолка #телеграм #телеграмм #telegram
+Присоединяйся, чтобы не потерять!!!
+реклама Челны вакансии
+Группы городов России опубликованы здесь!
+папки в телеграмме';
+$lang['show_path']             = 'Группа объявления Набережные челны в telegram.
+
+Постинг частных объявлений бесплатно!
+
+Рекламные и коммерческие объявления, 
+по правилам группы.
+#НабережныеЧелны #ОбъявленияНабережныеЧелны #Челны #БесплатныеОбъявления #объявление #доскаобъявлений #барахолка #телеграм #телеграмм #telegram
+Присоединяйся, чтобы не потерять!!!
+реклама Челны вакансии
+Группы городов России опубликованы здесь!
+папки в телеграмме';
