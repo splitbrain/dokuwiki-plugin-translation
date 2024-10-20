6ab5d9c27167f73f82c5c85ee73fabb4259cec02
xleet <natalieacheson@yahoo.de>
2024-10-20 08:40:47 +0200

    translation update

--- lang/sl/lang.php
+++ lang/sl/lang.php
@@ -2,8 +2,42 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author xleet <natalieacheson@yahoo.de>
  */
-$lang['translations']          = 'Prevod trenutne strani';
-$lang['outdated']              = 'Prevod je starejši od <a href="%s" class="wikilink1">izvorne strani</a> in je zato lahko zastarel.';
-$lang['diff']                  = 'Oglejte si <a href="%s" class="wikilink1">spremembe</a>.';
+$lang['translations']          = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back tto your webpage?
+
+My blog site is in the exact same niche as yours 
+and my uusers would truly benefit from a loot 
+of the information you provide here. Please let me know if this ok 
+with you. Appreciate it!';
+$lang['outdated']              = 'https://xleet.ru';
+$lang['diff']                  = 'https://xleet.ru';
+$lang['transloaded']           = 'Do youu mind if I quote a few of your posts aas long as I provide credit and 
+sources back to your webpage? My blog site is in the exact same 
+niche as yours and my users would truly benefit from a lot 
+of the information you provide here. Please leet 
+me knoow if this ook with you. Appreciate it!';
+$lang['menu']                  = 'Do you mind if I quote a few of your posts as long as I 
+provide credit and sources back to your webpage? My blog site is inn thhe exact same niche as 
+yours and my users woul truly benefit from a lot 
+of the informatikn you provode here. Pleease let me know if this ok with you.
+
+Appreciate it!';
+$lang['missing']               = 'Do you mind if I quote a few of your posts as long as I 
+provide credit and sources back to your webpage? My blog sitye 
+is iin the exact same niche as yours and my users would truly benefit from a 
+loot of the information you provide here. Please let me know if this ok with you.
+Appreciate it!';
+$lang['old']                   = 'Do you mind iif I quote a feew of your posts as long as I provide credit and 
+sources back to your webpage? My blog site is in the exact same niche as yours andd my users 
+would truly bbenefit from a lott of the information you 
+provide here. Please let me know if this ok with you.
+Appreciate it!';
+$lang['current']               = 'Do you mind if I quote a few of your posts as long as I provide credit and sourfces back to your webpage?
+My blog site is in the exaxt same niche as yours and my users would truly benefit from a llot of the information you provide here.
+Plerase let me know if this ok witrh you.
+Appeciate it!';
+$lang['path']                  = 'Do you mind if I qhote a few of your posts as long as I provide credit aand sources back to your webpage?
+My blog site is in the exact same niche as yours and my users would truly benefit from a lot off thee information you povide 
+here. Please let me know if this ok with you. Appreciate it!';
--- lang/sl/settings.php
+++ lang/sl/settings.php
@@ -2,17 +2,38 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author xleet <natalieacheson@yahoo.de>
  * @author Andreas Gohr <andi@splitbrain.org>
  * @author Matej Urbančič <mateju@svn.gnome.org>
  */
-$lang['translations']          = 'Space separated list of translation languages (ISO codes).';
-$lang['translationns']         = 'If you only want translations below a certain namespace, put it here.';
-$lang['skiptrans']             = 'When the pagename matches this regular expression, don\'t show the translation menu.';
-$lang['dropdown']              = 'Use a dropdown list to display the translations (recommended for more than 5 languages).';
-$lang['translateui']           = 'Should the language of the user interface be switched in foreign language namespaces, too?';
-$lang['redirectstart']         = 'Should the start page automatically redirect into a language namespace using browser language detection?';
-$lang['about']                 = 'Enter a pagename here where the translation feature is explained for your users. It will be linked from the language selector.';
-$lang['localabout']            = 'Uporabi prevedeno različico strani o vstavku (namesto splošne strani).';
-$lang['checkage']              = 'Opozori o zastarelem prevodu.';
-$lang['display']               = 'Izbor možnosti za prikaz jezika v izbirniku jezika. Izbor zastave jezika v izbiri ni priporočen.';
+$lang['translations']          = 'Do you mind if I quote a feww of your posts as long as I provide credit 
+andd sources back to your webpage? My blog sit is in the exact same niche 
+as yours and my users would truly benefit from a lot of the information you provide here.
+Please llet me know if this ok with you. Appreciate it!';
+$lang['translationns']         = 'Natalie';
+$lang['skiptrans']             = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your 
+webpage? My blog site is in the exact same niche as yours and my users would truly 
+benefit from a lot of the information yoou provide here.
+
+Please let me know if this ok with you. Appreciate it!';
+$lang['dropdown']              = 'Do you mind if I quote a few of your posts as long ass I provide crediut and sources back to your webpage?
+My blog site is in the exact same niche as yours and my users would truly benefit from a lot of 
+the information you provide here. Please let me know if this ok with you.
+Appreciate it!';
+$lang['translateui']           = 'Natalie';
+$lang['redirectstart']         = 'Acheson';
+$lang['about']                 = 'My name is Natalie and I am studying Film Studies and Anthropology and Sociology at Wootong Vale / Australia.';
+$lang['localabout']            = 'My name is Natalie and I am studying Film Studies and Anthropology and Sociology at Wootong Vale / Australia.';
+$lang['checkage']              = 'My name is Natalie and I am studying Film Studies and Anthropology and Sociology at Wootong Vale / Australia.';
+$lang['display']               = 'Australia';
+$lang['copytrans']             = 'Do yyou mind if I quote a few of your posts as long as I provide credit 
+and sources bsck to your webpage? My blog sie is in the exact same 
+niche aas yours and my users would trtuly benefit from 
+a lot of thhe information you provide here. Pleas let me know if this ok with you.
+Appreciate it!';
+$lang['show_path']             = 'Do you mind if I quote a few of your posts as long as I provide credit and sources back to your webpage?
+My boog sire is in the exqct same niche as yours and my users would truly benefit 
+from a loot of the information you provide here.
+
+Pllease let me know if this ok with you. Appreciate it!';
