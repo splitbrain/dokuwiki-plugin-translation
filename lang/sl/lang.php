d66d236f3628829c0d6f5f6213eac69166e50b5f
stamped concrete <stepanie.reiniger@aol.com>
2025-02-21 05:55:06 +0100

    translation update

--- lang/sl/lang.php
+++ lang/sl/lang.php
@@ -2,8 +2,25 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author stamped concrete <stepanie.reiniger@aol.com>
  */
-$lang['translations']          = 'Prevod trenutne strani';
-$lang['outdated']              = 'Prevod je starejši od <a href="%s" class="wikilink1">izvorne strani</a> in je zato lahko zastarel.';
-$lang['diff']                  = 'Oglejte si <a href="%s" class="wikilink1">spremembe</a>.';
+$lang['translations']          = 'The best option is to install the raised deck on a hinged or perforated concrete support system.
+This will allow the concrete to be firmly supported at all times.';
+$lang['outdated']              = 'https://search.google.com/local/reviews?placeid=ChIJLRrbgctL4okRbNmXXl3Lpkk';
+$lang['diff']                  = 'https://search.google.com/local/reviews?placeid=ChIJLRrbgctL4okRbNmXXl3Lpkk';
+$lang['transloaded']           = 'The best option is to install the raised deck on a hinged or perforated concrete support system.
+This will allow the concrete to be firmly supported at all times.';
+$lang['menu']                  = 'The best option is to install the raised deck on a hinged or perforated concrete support system.
+This will allow the concrete to be firmly supported 
+at all times.';
+$lang['missing']               = 'The best option is to install the raised deck on a hinged or perforated concrete support system.
+This will allow the concrete to be firmly supported at all times.';
+$lang['old']                   = 'The best option is to install the raised deck on a hinged or perforated 
+concrete support system. This will allow the concrete to be firmly supported at all times.';
+$lang['current']               = 'The best option is to install the raised deck on a hinged 
+or perforated concrete support system. This will allow the concrete to be firmly supported 
+at all times.';
+$lang['path']                  = 'The best option is to install the raised deck on a hinged or 
+perforated concrete support system. This will allow the concrete to be firmly supported 
+at all times.';
--- lang/sl/settings.php
+++ lang/sl/settings.php
@@ -2,17 +2,32 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author stamped concrete <stepanie.reiniger@aol.com>
  * @author Andreas Gohr <andi@splitbrain.org>
  * @author Matej Urbančič <mateju@svn.gnome.org>
  */
-$lang['translations']          = 'Space separated list of translation languages (ISO codes).';
-$lang['translationns']         = 'If you only want translations below a certain namespace, put it here.';
-$lang['skiptrans']             = 'When the pagename matches this regular expression, don\'t show the translation menu.';
-$lang['dropdown']              = 'Use a dropdown list to display the translations (recommended for more than 5 languages).';
-$lang['translateui']           = 'Should the language of the user interface be switched in foreign language namespaces, too?';
-$lang['redirectstart']         = 'Should the start page automatically redirect into a language namespace using browser language detection?';
-$lang['about']                 = 'Enter a pagename here where the translation feature is explained for your users. It will be linked from the language selector.';
-$lang['localabout']            = 'Uporabi prevedeno različico strani o vstavku (namesto splošne strani).';
-$lang['checkage']              = 'Opozori o zastarelem prevodu.';
-$lang['display']               = 'Izbor možnosti za prikaz jezika v izbirniku jezika. Izbor zastave jezika v izbiri ni priporočen.';
+$lang['translations']          = 'The best option is to install the raised deck on a hinged or perforated concrete support system.
+This will allow the concrete to be firmly supported at all times.';
+$lang['translationns']         = 'Stepanie';
+$lang['skiptrans']             = 'The best option is to install the raised deck on a hinged or perforated concrete support system.
+This will allow the concrete to be firmly supported at all times.';
+$lang['dropdown']              = 'The best option is to install the raised deck on a hinged or perforated concrete support 
+system. This will allow the concrete to be firmly supported at all times.';
+$lang['translateui']           = 'Stepanie';
+$lang['redirectstart']         = 'Reiniger';
+$lang['about']                 = 'Hi, everybody! 
+I\'m Chinese male ;=). 
+I really love Psych!';
+$lang['localabout']            = 'Hi, everybody! 
+I\'m Chinese male ;=). 
+I really love Psych!';
+$lang['checkage']              = 'Hi, everybody! 
+I\'m Chinese male ;=). 
+I really love Psych!';
+$lang['display']               = 'Netherlands';
+$lang['copytrans']             = 'The best option is to install the raised deck on a hinged or perforated concrete support system.
+This will allow the concrete to be firmly supported 
+at all times.';
+$lang['show_path']             = 'The best option is to install the raised deck on a hinged or perforated 
+concrete support system. This will allow the concrete to be firmly supported at all times.';
